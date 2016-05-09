c9518ff73f7ec7c7e569d4cf130c37d20c104416
jinkimdata <jinkimdata@gmail.com>
2016-05-09 17:01:01 -0400

    Comments added

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -3,6 +3,7 @@ var preaknessContenders = {
 		preaknessContenders.share();
 		preaknessContenders.scrollFunctions();		
 	},
+	// Share tool functions.
 	share: function(){
 		$(".icon-twitter").on("click", function(){
 			var tweet = "Prep for this year's Preakness Stakes with this handy breakdown of the contenders"; //Tweet text
@@ -19,11 +20,14 @@ var preaknessContenders = {
 			window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
 		});
 	},
+	// All functions relating to scrolling on the site is here.
 	scrollFunctions: function() {
+		// Except this one. This one gets rid of the splash page.
 		$('.startButton').on('click touchend', function() {
 			$('.splash').fadeOut();
 			$('.hidden').removeClass('hidden');
 		});
+		// iScroll
 		var myScroll = new IScroll('#contenders', {
 			scrollX: true,
 			scrollY: true,
@@ -37,17 +41,21 @@ var preaknessContenders = {
 		document.addEventListener('touchend', function (e) { 
 			e.preventDefault(); 
 		}, false);
+		// The timer puts on a cap on the number of times functions trigger. It is used repeatedly through this code.
 		var timer;
 		myScroll.on('scroll',function(){
+			// It works by checking if a "timer" exists. If it does, nothing happens.
 			if (timer) {
 				clearTimeout(timer);
 			};
+			// What is a timer? It's just a function that triggers after a delay.
 			timer = setTimeout(function() {
 				if (myScroll.currentPage.pageY < 2) {
 					sticky_relocate();
 				}
-			},50);
+			}, 50);
 		});
+		// This moves the left and right arrows that overlay on the page from the center to the top.
 		function sticky_relocate() {
 			if (myScroll.currentPage.pageY > 0) {
 				$('#sticky').addClass('stick');
@@ -57,7 +65,9 @@ var preaknessContenders = {
 				$('#sticky-anchor').height(0);
 			};
 		};
-
+		// All other button functions are below.
+		//
+		// These are for the arrows. It pulls the data-dir attribute to see what direction to move the page.
 		$('.fa').on('click touchend', function(e){
 			var dir = $(this).attr('data-dir');			
 			if (timer) {
@@ -90,7 +100,7 @@ var preaknessContenders = {
 				};
 			}, 50);
 		});
-
+		// I wonder what this one does?
 		$('.backToTop').on('click touchend', function(e){
 			if (timer) {
 				clearTimeout(timer);
@@ -99,6 +109,7 @@ var preaknessContenders = {
 				myScroll.goToPage(myScroll.currentPage.pageX, 0);
 			}, 50);
 		});
+		// These are events for the navbar.
 		$('.navBlock').on('click touchend', function(e){
 			var pos = Number($(this).attr('data-pos'));
 			if (timer) {
@@ -108,6 +119,7 @@ var preaknessContenders = {
 				myScroll.goToPage(myScroll.currentPage.pageX, pos);
 			}, 50);
 		});
+		// At the end of every scroll event, update the navbar to highlight the appropraite page.
 		myScroll.on('scrollEnd',function(){			
 			$('.selected').removeClass('selected');
 			$('.navBlock--'+myScroll.currentPage.pageY).addClass('selected');
